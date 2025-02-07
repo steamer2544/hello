c3b3b7e873fd7dee93231e122df0e5c3c6bf22e2
Ratthakorn <steamer2544@gmail.com>
2025-02-07 17:59:43 +0700

    shopping cart

--- app.py
+++ app.py
@@ -1,38 +1,12 @@
 from flask import Flask, render_template, request
-from cs50 import SQL
+from flask_session import Session
 
 app = Flask(__name__)
 
-db = SQL("sqlite:///froshims.db")
-
-SPORTS = [
-    "Basketball",
-    "Soccer",
-    "Ultimate Frisbee",
-]
-
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app) # turn on session
 
 @app.route("/")
 def index():
-    return render_template('index.html', sports=SPORTS)
-
-@app.route("/register", methods=["POST"])
-def register():
-    name = request.form.get("name")
-    if not name:
-        return render_template("error.html", message="Missing name")
-    
-    sport = request.form.get("sport")
-    if not sport:
-        return render_template("error.html", message="Missing sport")
-    if sport not in SPORTS:
-        return render_template("error.html", message="Invalid sport")
-    
-    db.execute("INSERT INTO registrants (name, sport) VALUES(?, ?)", name, sport)
-    
-    return render_template("success.html")
-
-@app.route("/registrants")
-def registrants():
-    registrants = db.execute("SELECT * FROM registrants")
-    return render_template("registrants.html", registrants=registrants)
+    return render_template("index.html")
